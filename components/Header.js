f2a5dc64a76ed4a9aac29cf96004dcf8d9efe7d0
FaiqaNasreen <nasreeakhter44@gmail.com>
2024-06-09 20:17:13 -0700

    add commit

---
+++ components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./../App.css";
+function Header() {
+  return (
+    <div>
+      <div className='App'>
+        <header class="main-header clearfix" role="header">
+    <div class="logo">
+        <a href="#"><em>Welcom ToDo list</em></a>
+    </div>
+    <a href="#menu" class="menu-link"><i class="fa fa-bars"></i></a>
+    <nav id="menu" class="main-nav" role="navigation">
+        <ul class="main-menu">
+            <li><a href="#section1"><em>Home</em></a></li>
+            <li class="has-submenu"><a href="#section2"><em>About Us</em></a>
+                <ul class="sub-menu">
+                    <li><a href="#section2">Who we are?</a></li>
+                    <li><a href="#section3">What we do?</a></li>
+                    <li><a href="#section3">How it works?</a></li>
+                    <li><a href="https://templatemo.com/about" rel="sponsored" class="external">External URL</a></li>
+                </ul>
+            </li>
+            <li><a href="#"><em>Courses</em></a></li>
+            
+            <li><a href="#"><em>Contact</em></a></li>
+            <li><a href="#" class="external"><em>External</em></a></li>
+        </ul>
+    </nav>
+</header>
+</div>
+
+    </div>
+  )
+}
+
+export default Header
